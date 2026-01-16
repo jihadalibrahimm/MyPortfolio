8e475fb6acbc4f8aaefecfdf7a6be4e66b35e22d
jihadalibrahimm <81075046+jihadalibrahimm@users.noreply.github.com>
2026-01-16 11:26:42 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,6 +11,7 @@ import { Footer } from "../components/Footer"
 export const Home = ()=>{
     return(
         <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+            NEWWWWWWWWWWWWW
             {/* Theme Toggle */}
             <ThemeToggle mobile={false} />
             {/* Background Effects */}
